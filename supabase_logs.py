4c35c0a9ceb998296323703eaa15c7b47e2b012e
Alexey Leshchenko <leshchenko@gmail.com>
2024-03-15 15:42:38 +0300

    Save time by not writing `skipped` and `already sent` into the database

--- supabase_logs.py
+++ supabase_logs.py
@@ -24,16 +24,19 @@ class SupabaseLogHandler:
         return result.data[0] if result.data else None
 
     def add_log_entry(self, client_name: str, setting: settings.Setting, result: str):
-        # Add log entry
-        entry = {
-            "client_name": client_name,
-            "account": setting.account,
-            "chat_id": setting.chat_id,
-            "result": result,
-            "setting_unique_id": setting.get_hash(),
-        }
+        # Save time by not writing `skipped` and `already sent`
+        #into the database
+        if "skipped" not in result and "already sent" not in result:
+            entry = {
+                "client_name": client_name,
+                "account": setting.account,
+                "chat_id": setting.chat_id,
+                "result": result,
+                "setting_unique_id": setting.get_hash(),
+            }
 
-        self.supabase_client.table("log_entries").insert(entry).execute()
+            self.supabase_client.table("log_entries").insert(entry).execute()
 
+        # Log errors as warnings for easier search in the log
         method = logger.warning if "error" in result.lower() else logger.info
         method(f"Logged {entry}", extra=entry)
